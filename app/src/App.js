089adb5f5795c75e5efb7c08d87f2ef7fdabbb5d
gimenomarc <gimenocervantesmarc@gmail.com>
2023-06-17 19:19:28 +0200

    favicon etc

--- app/src/App.js
+++ app/src/App.js
@@ -13,7 +13,7 @@ import TRAnorexiaNerviosaPage from './pages/TR_anorexiaNerviosa';
 import TRBulimiaNerviosaPage from './pages/TR_bulimiaNerviosa';
 import TRFartaneresPage from './pages/TR_fartaneres';
 import LeyProteccionDatosPage from './pages/proteccioDades';
-
+import Header from './components/Header'
 
 function App() {
   return (
@@ -24,15 +24,16 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/datos-personales" element={<DatosPersonales />} />
         <Route path="/quiz" element={<Quiz />} />
-        <Route path="/infoTCA" element={<InfoTCA />} />
+        <Route path="/infoTCA/*" element={<InfoTCA />} />
         <Route path="/infoTCA/pica" element={<TRPicaPage />} />
         <Route path="/infoTCA/trastorn-ruminacio" element={<TRRuminacioPage />} />
         <Route path="/infoTCA/trastorn-ingesta-aliments" element={<TRIngestaAlimentsPage />} />
         <Route path="/infoTCA/anorexia-nerviosa" element={<TRAnorexiaNerviosaPage />} />
         <Route path="/infoTCA/bulimia-nerviosa" element={<TRBulimiaNerviosaPage />} />
         <Route path="/infoTCA/trastorn-fartaneres" element={<TRFartaneresPage />} />  
-        <Route path="/llei-proteccio-dades" element={<LeyProteccionDatosPage />} />      
+        <Route path="/llei-proteccio-dades" element={<LeyProteccionDatosPage />} />
       </Routes>
+      <Header />
     </Router>
   );
 }
---
+++ app/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-gray-700 p-3 text-white text-sm flex justify-center items-center">
+      <div className="text-center">
+        <h1 className="font-bold">Treball de final de grau</h1>
+        <p className="mb-0">Projecte de recerca en Metodologia</p>
+        <p className="mb-0">Núria Llurba Puig</p>
+        <p className="mb-0">Grau de psicologia, UOC</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
